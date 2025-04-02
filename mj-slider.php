aad605978fed96cdf22a2dfc9ae2418c659d0b94
mjaben <alexaben291@gmail.com>
2025-04-02 19:24:40 +0100

    Creating Plugin class

--- mj-slider.php
+++ mj-slider.php
@@ -14,6 +14,32 @@
  * Domain Path: /languages
  */
 
+/*
+MJ Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+MJ Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MJ Slider. If not, see http://www.gnu.org/licenses/gpl-2.0.html.
+*/
  if(! defined('ABSPATH')){
     exit;
  }
+
+ if (! class ('MJ_Slider')){
+   class MJ_SLIDER {
+      function __construct(){
+
+      }
+   }
+ }
+
+if (class_exist ('MJ_Slider')){
+   $mj_slider = new MJ_Slider();
+}
